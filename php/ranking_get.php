5c736eb041b139886812c4d7cba859ceb3a039db
Merry-Amor <nagartyuuiti@gmail.com>
2021-07-31 11:42:37 +0900

    まだbindValueにエラーがあるものの、ランキングにINSERTする処理を作成

--- php/ranking_get.php
+++ php/ranking_get.php
@@ -38,6 +38,54 @@ if(isset($_GET["ranking"]) && !preg_match('/[^0-9]/', $_GET["ranking"])) {
     // print_r($result_array);
     $arr["result"] = $result_array;
 
+} elseif(count($_GET) > 1) {
+
+  $get_array = array();
+
+  foreach ($_GET as $key => $value) {
+    $get_array[$key] = $value;
+  }
+
+  if (array_key_exists('token', $get_array)) {
+
+    if ($get_array["token"] === "MerryAmor") {
+
+      try {
+
+        $arr["status"] = "yes";
+        $db = new SQLite3($db_name);
+
+      } catch (Exception $e) {
+
+        $arr["status"] = "no";
+        $arr["error"] = "DB接続エラー";
+
+        print json_encode($arr, JSON_PRETTY_PRINT);
+      }
+
+      $stmt = $db->prepare("INSERT INTO ranking (name, score) VALUES (?,?);");
+
+      $stmt->bindValue(1, $get_array["name"], SQLITE3_TEXT);
+      $stmt->bindValue(2, $get_array["score"], SQLITE3_TEXT);
+      $results = $stmt->execute();
+
+      $result_array = array();
+
+
+    } else {
+
+      $arr["status"] = "no";
+      $arr["error"] = "認証エラー";
+
+    }
+
+  } else {
+
+    $arr["status"] = "no";
+    $arr["error"] = "認証エラー";
+
+  }
+
 } else {
     $arr["status"] = "no";
 }
